3c187127f41aa96a69d71c4ac55ef745bd193bbf
matheus <matheuslima5926@gmail.com>
2021-04-10 18:44:05 -0300

    inlcude JWT basic logic

--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -1,3 +1,10 @@
 class AuthenticationTokenService
+    HMAC_SECRET = "soningBlaning"
+    ALGORITHM_TYPE = 'HS256'
 
+    def self.call
+        payload = {"test" => "test_token "}
+       JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
+    end
+    
 end
--- spec/services/authentication_token_spec.rb
+++ spec/services/authentication_token_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 describe AuthenticationTokenService do 
-    describe 'returns authentication token' do
-        
+    describe '.call' do
+        let(:token) {described_class.call}
+        it 'returns authentication token' do
+            decoded_token = JWT.decode(
+                token,
+                AuthenticationTokenService::HMAC_SECRET,
+                true,
+                {algorithm: AuthenticationTokenService::ALGORITHM_TYPE}
+            )
+            expect(decoded_token).to eq(
+                [{"test"=>"test_token "}, {"alg"=>"HS256"}]
+            )
+        end
     end
 end
