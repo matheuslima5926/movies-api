e3cf34a2cd9225630d09805126c4845fdf461c81
matheus <matheuslima5926@gmail.com>
2021-04-10 17:55:42 -0300

    tests for authentication

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::AuthenticationController < ApplicationController
     def create
+        render json: {token: 'user_token123'}, status: :created
     end
+
+    private
+        def auth_params
+            params.require(:auth).permit(:username, :password)
+        end
 end
--- spec/requests/api/v1/authentication_spec.rb
+++ spec/requests/api/v1/authentication_spec.rb
@@ -2,8 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Authentications", type: :request do
   describe 'POST auth' do 
-    it 'authenticate user' do
-        post '/api/v1/auth'
+    it 'authenticate user with correct params' do
+        post '/api/v1/auth', params: {username: 'user', password: 'psdUser'}
+        expect(response).to have_http_status(:created)
+        expect(JSON.parse(response.body)).to eq({
+          'token' => 'user_token123'
+        })
+    end
+    it 'return authentication error when username is missing' do
+      post '/api/v1/auth', params: {password: 'psdUser'}
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  
+    it 'return authentication error when password is missing' do
+      post '/api/v1/auth', params: {username: 'user'}
+      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
 end
