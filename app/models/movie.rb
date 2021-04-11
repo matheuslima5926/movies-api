420fca3ec4a142d632a2465c3d7d10406b868477
matheus <matheuslima5926@gmail.com>
2021-04-11 18:30:00 -0300

    fix association for movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,8 @@
 class Movie < ApplicationRecord
     validates_presence_of :original_title, :release_date, :director, :gender
-    has_many :average_scores
-    has_many :actor_movies
+    has_many :average_scores, dependent: :destroy
+    has_many :actor_movies, dependent: :destroy
     has_many :actors, :through => :actor_movies
-
     def to_view
         return {id: id, original_title: original_title, release_date: release_date, director: director, gender: gender, users_average: users_avg_score, cast: actors.pluck(:name) }
     end
