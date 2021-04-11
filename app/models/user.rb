93374b84802a109562aeb7ca908c08de7606f773
matheus <matheuslima5926@gmail.com>
2021-04-11 19:52:14 -0300

    fix model validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_secure_password
     validates :email, uniqueness: {message: "email has already been taken"}
     validates_presence_of :email
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "invalid email" }
+    validates :password, :presence => true, :length => {:within => 6..40, :message => "password must have length between 6 and 40 characters"}
     def to_view
         return {user_id: id, email: email, created_at: created_at, updated_at: updated_at} if !admin
         return {user_id: id, email: email, created_at: created_at, updated_at: updated_at, admin: admin}
