c6c70129082bedb1467c36de3d6349fd552f4583
matheus <matheuslima5926@gmail.com>
2021-04-10 22:18:16 -0300

    include admin flag for user

--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -6,5 +6,15 @@ class AuthenticationTokenService
         payload = {user_id: user_id}
        JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
     end
+
+    def self.decode_token(token)
+       decoded_token = JWT.decode(token, HMAC_SECRET, true, {algorith: ALGORITHM_TYPE})
+       decoded_token[0]['user_id']
+    end
+
+    def self.find_user_by_token(token)
+        user_id = decode_token(token)
+        return User.find_by(id: user_id)
+    end
     
 end
