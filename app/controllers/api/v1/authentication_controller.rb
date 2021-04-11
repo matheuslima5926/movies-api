a89be7a6deb64394d0181689bda84dba3c50536e
matheus <matheuslima5926@gmail.com>
2021-04-10 23:16:28 -0300

    fix strong params for authentication_controller

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::AuthenticationController < ApplicationController
     def create
-        user = User.find_by(email: params[:email])
+        user = User.find_by(email: auth_params[:email])
         jwt = "Bearer #{AuthenticationTokenService.call(user.id)}"
         render json: {jwt: jwt}, status: :created
     end
 
     private
         def auth_params
-            params.require(:auth).permit(:email, :password)
+            params.permit(:email, :password)
         end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'auth', to: 'authentication#create'
+      resources :users, only:[:create]
     end
   end
 end
