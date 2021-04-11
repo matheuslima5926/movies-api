513287224f4e6d01fa5d37345127fadbccdf04f5
matheus <matheuslima5926@gmail.com>
2021-04-11 19:19:21 -0300

    Revert "fix error in date parse"
    This reverts commit 6af13140a61743e0251c256ff4883df800dc50b6.

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,6 +1,6 @@
 class MovieService
     def self.create_movie(original_title, release_date, director, gender)
-        movie = Movie.create(original_title: original_title, release_date: Date.parse(release_date), director: director, gender: gender)
+        movie = Movie.create(original_title: original_title, release_date: release_date, director: director, gender: gender)
         if movie.save
             return movie.to_view
         end
